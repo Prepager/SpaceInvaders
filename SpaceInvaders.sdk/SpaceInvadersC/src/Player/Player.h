eafb335f77f1be9968dff0fe5cfd7019d4033a54
Andreas <andreas@zapsterstudios.com>
2019-01-11 12:45:44 +0100

    Made player ship draw

--- SpaceInvaders.sdk/SpaceInvadersC/src/Player/Player.h
+++ SpaceInvaders.sdk/SpaceInvadersC/src/Player/Player.h
@@ -2,6 +2,7 @@
 #define SRC_PLAYER_PLAYER_H_
 
 // Includes
+#include "xil_io.h"
 #include "xil_types.h"
 #include "../settings.h"
 
@@ -16,5 +17,10 @@ typedef struct {
 	// Positions
 	u32 position;
 } Player;
+
+// Constructors
+void positionPlayer(Player *player);
+void paintPlayer(Player *player, u8 *frame);
+void depaintPlayer(Player *player, u8 *frame);
 
 #endif
--- SpaceInvaders.sdk/SpaceInvadersC/src/game.c
+++ SpaceInvaders.sdk/SpaceInvadersC/src/game.c
@@ -6,6 +6,7 @@ u8 buffer[DISPLAY_NUM_FRAMES][MAX_FRAME];
 u8 *bufferPointer[DISPLAY_NUM_FRAMES];
 
 // Object arrays.
+Player player;
 Enemy enemies[ENEMY_ROWS*ENEMY_COLS];
 
 // todo
@@ -49,15 +50,24 @@ void renderScene()
 	// Get the current frame.
 	u8 *frame = controller.framePtr[controller.curFrame];
 
-	// Draw the background.
+	// Draw the background if needed.
 	if (drawBackground) {
+		// Draw the background.
 		memset(&frame[0], BACKGROUND, sizeof(u8) * MAX_FRAME);
+
+		// Disable background drawing.
+		drawBackground = 0;
 	}
 
 	// Position and paint enemies.
 	depaintEnemies(enemies, frame);
 	positionEnemies(enemies);
 	paintEnemies(enemies, frame);
+
+	// Position and paint player.
+	depaintPlayer(&player, frame);
+	positionPlayer(&player);
+	paintPlayer(&player, frame);
 
 	// Flush the frame cache.
 	Xil_DCacheFlushRange((unsigned int) frame, MAX_FRAME);
